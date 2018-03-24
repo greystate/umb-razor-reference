01331f7dd1b8c0ded3f5dfa866c86122bf61f750
Chriztian Steinmeier <chriztian@steinmeier.dk>
2018-03-24 14:14:53 +0100

    Add build steps for the docset bundle

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 DOCSET_NAME="Umbraco Razor Quick Reference"
 ARCHIVE_NAME="Umbraco_Razor_Quick_Reference"
-BUNDLE="dist/$DOCSET_NAME.docset"
 DOC_XML_FILE="src/data/razor-reference.xml"
 FEED_FILENAME="umbraco-razor-quick-reference"
 TEMP_DIR="dist/temp"
 DIST_DIR="dist"
+BUNDLE="$DIST_DIR/$DOCSET_NAME.docset"
 
 # Build the resulting file
 xsltproc --xinclude -o "$TEMP_DIR/index.html" src/xslt/build-dash-docset.xslt "$DOC_XML_FILE"
@@ -12,7 +12,26 @@ xsltproc --xinclude -o "$TEMP_DIR/index.html" src/xslt/build-dash-docset.xslt "$
 # Build the SQL to populate the docSet.dsidx SQLLite database
 xsltproc --xinclude -o "$TEMP_DIR/docs.sql" src/xslt/build-sql.xslt "$DOC_XML_FILE"
 
-
 # Build the Dash DocSet feed XML file
 xsltproc -o "$TEMP_DIR/$FEED_FILENAME.xml" --stringparam filename "$ARCHIVE_NAME" src/xslt/build-feed.xslt "$DOC_XML_FILE"
+
+# Backup the existing DocSet (if any)
+if [[ -d "$BUNDLE" ]]; then
+	rm -rf "$BUNDLE.bak"
+	mv "$BUNDLE" "$BUNDLE.bak"
+fi
+
+# Create the Docset Folder
+mkdir -p "$BUNDLE/Contents/Resources/Documents/"
+
+# Copy HTML + assets
+cp $TEMP_DIR/index.html "$BUNDLE/Contents/Resources/Documents"
+cp build/assets/app.css "$BUNDLE/Contents/Resources/Documents"
+cp build/assets/app.min.js "$BUNDLE/Contents/Resources/Documents"
+
+# Copy the Info.plist file
+cp "lib/Info.plist" "$BUNDLE/Contents"
+
+# Copy the icon
+cp "lib/icon.png" "$BUNDLE"
 
