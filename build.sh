bb57600110c6975ffc17d2a1e6bd5f047892c2b9
Chriztian Steinmeier <chriztian@steinmeier.dk>
2018-03-24 13:51:24 +0100

    Start the build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+DOCSET_NAME="Umbraco Razor Quick Reference"
+ARCHIVE_NAME="Umbraco_Razor_Quick_Reference"
+BUNDLE="dist/$DOCSET_NAME.docset"
+DOC_XML_FILE="src/data/razor-reference.xml"
+FEED_FILENAME="umbraco-razor-quick-reference"
+DIST_DIR="dist"
+
+# Build the resulting file
+xsltproc --xinclude -o "$DIST_DIR/index.html" src/xslt/build-dash-docset.xslt "$DOC_XML_FILE"
+
+# Build the SQL to populate the docSet.dsidx SQLLite database
+xsltproc --xinclude -o dist/docs.sql src/xslt/build-sql.xslt "$DOC_XML_FILE"
+
