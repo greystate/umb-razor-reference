5a27e99d538c60e7e82367feb871a86b2404abaf
Chriztian Steinmeier <chriztian@steinmeier.dk>
2018-03-24 14:00:09 +0100

    Add build step for the Dash feed XML file

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ xsltproc --xinclude -o "$DIST_DIR/index.html" src/xslt/build-dash-docset.xslt "$
 # Build the SQL to populate the docSet.dsidx SQLLite database
 xsltproc --xinclude -o dist/docs.sql src/xslt/build-sql.xslt "$DOC_XML_FILE"
 
+
+# Build the Dash DocSet feed XML file
+xsltproc -o "dist/$FEED_FILENAME.xml" --stringparam filename "$ARCHIVE_NAME" src/xslt/build-feed.xslt "$DOC_XML_FILE"
+
