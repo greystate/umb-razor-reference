45d364088cdb909b1f6bc6b58238a485b5119e04
Chriztian Steinmeier <chriztian@steinmeier.dk>
2018-03-24 14:03:13 +0100

    Build into a temp directory

--- build.sh
+++ build.sh
@@ -3,15 +3,16 @@ ARCHIVE_NAME="Umbraco_Razor_Quick_Reference"
 BUNDLE="dist/$DOCSET_NAME.docset"
 DOC_XML_FILE="src/data/razor-reference.xml"
 FEED_FILENAME="umbraco-razor-quick-reference"
+TEMP_DIR="dist/temp"
 DIST_DIR="dist"
 
 # Build the resulting file
-xsltproc --xinclude -o "$DIST_DIR/index.html" src/xslt/build-dash-docset.xslt "$DOC_XML_FILE"
+xsltproc --xinclude -o "$TEMP_DIR/index.html" src/xslt/build-dash-docset.xslt "$DOC_XML_FILE"
 
 # Build the SQL to populate the docSet.dsidx SQLLite database
-xsltproc --xinclude -o dist/docs.sql src/xslt/build-sql.xslt "$DOC_XML_FILE"
+xsltproc --xinclude -o "$TEMP_DIR/docs.sql" src/xslt/build-sql.xslt "$DOC_XML_FILE"
 
 
 # Build the Dash DocSet feed XML file
-xsltproc -o "dist/$FEED_FILENAME.xml" --stringparam filename "$ARCHIVE_NAME" src/xslt/build-feed.xslt "$DOC_XML_FILE"
+xsltproc -o "$TEMP_DIR/$FEED_FILENAME.xml" --stringparam filename "$ARCHIVE_NAME" src/xslt/build-feed.xslt "$DOC_XML_FILE"
 
